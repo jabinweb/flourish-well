1c1c5ed34899203fd96fdcf698ef80b8a912ff8a
Harshit Singh <jabincreators@gmail.com>
2025-10-18 20:29:24 +0530

    first commit

--- components/gallery-section.tsx
+++ components/gallery-section.tsx
@@ -149,8 +149,8 @@ export function GallerySection() {
           ))}
         </div>
 
-        {/* Gallery Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-6">
+  {/* Gallery Grid */}
+  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-6">
           {filteredImages.map((image, index) => (
             <Card
               key={image.id}
